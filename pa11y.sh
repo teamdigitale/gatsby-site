0c7601c8b5fa847f590018a17cff56790cdb3fab
Fabio Bonelli <fb@fabiobonelli.it>
2021-07-23 10:30:06 +0200

    feat: stricter mode in pa11y.sh

--- pa11y.sh
+++ pa11y.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-set -e
 
-# http-equiv="refresh" to skip redirect pages
+set -euxo pipefail
+
+# Skip redirect pages
 find public -type f -name '*.html' -exec grep -L 'http-equiv="refresh"' {} \; | xargs yarn pa11y-ci
